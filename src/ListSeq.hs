4c22f1107fb6efb11b8878057d6a8ac65f77c9c9
A-Cabrera-I <cabrera.alvaro155@gmail.com>
2026-06-21 00:09:31 -0300

    inciso a) y b), primer esqquema

--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -27,13 +27,35 @@ instance Seq [] where
     joinS [] = []
     joinS (xs:xss) = xs `appendS` joinS xss
 
+    reduceS f b [] = b
+    reduceS f b [x] = f b x
+    reduceS f b xs = reduceS f b (comprimir xs)
+        where comprimir [] = []
+              comprimir [x] = [x]
+              comprimir (x:y:xs) = f x y : comprimir xs
+
+    scanS f b [] = ([], b)
+    scanS f b [x] = ([b], f b x)
+    scanS f b xs = 
+        let xs' = comprimir xs
+            (s', total) = scanS f b xs'
+            n = length xs
+            r = expandir s' xs
+                in (r, total)
+        where
+            comprimir [] = []
+            comprimir [x] = [x]
+            comprimir (x:y:xs) = f x y : comprimir xs
+
+            expandir [] [] = []
+            expandir 
+
 
     fromList = id                           --Prelude
-
-
 {-
-Alvaro:
+Alva:
 El tema de showtS:
-    no se s
-
+    no se si showtS y showlS tienen que ser recursivos, es decir:
+        Si al hacer showtS [1,2,3,4,5,6], devuelve NODE [1,2,3] [4,5,6]
+        Ese resultado es correcto , o deberia hacer un arbolito con todas hojas :p
 -}
